eb36e38203d3e54cfd47c6536cfc1dfbea62032a
Matthew Podwysocki <matthewp@microsoft.com>
2024-02-07 17:42:56 -0500

    Adding keyvault

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,14 @@
-import { OpenAIClient, AzureKeyCredential } from "@azure/openai";
+// Which sample to run
+import { main as openAiMain } from "./index.openai.js";
+import { main as keyvaultMain } from "./index.keyvault.js";
 
-// Load the .env file if it exists
-import * as dotenv from "dotenv";
-dotenv.config();
-
-// You will need to set these environment variables or edit the following values
-const endpoint = process.env["ENDPOINT"] || "<endpoint>";
-const azureApiKey = process.env["AZURE_API_KEY"] || "<api key>";
-
-const prompt = ["What is Azure OpenAI?"];
-
-export async function main() {
-  console.log("== Get completions Sample ==");
-
-  const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
-  const deploymentId = "text-davinci-003";
-  const result = await client.getCompletions(deploymentId, prompt, { maxTokens: 128 });
-
-  for (const choice of result.choices) {
-    console.log(choice.text);
+async function main() {
+  try {
+    await openAiMain();
+    await keyvaultMain();
+  } catch (err) {
+    console.error("The sample encountered an error:", err);
   }
 }
 
-main().catch((err) => {
-  console.error("The sample encountered an error:", err);
-});
+main();
